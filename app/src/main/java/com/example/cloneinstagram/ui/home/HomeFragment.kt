c00dd315f7949ee6f8fb1e89976e8531c9af00cf
Melker Halberd Pereira Alves <melker168@gmail.com>
2021-01-12 14:49:31 -0300

    Terminei a Toolbar da Home

--- app/src/main/java/com/example/cloneinstagram/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cloneinstagram/ui/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.example.cloneinstagram.R
 
 class HomeFragment : Fragment() {
@@ -15,13 +14,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_home, menu)
-
-    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,10 +24,6 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
 
         return root
     }
